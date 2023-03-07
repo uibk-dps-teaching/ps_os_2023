f44b6bd3fda856ec67695b0ffbfce0f43e0c09d4
Stefan Pedratscher <Stefan.Pedratscher@student.uibk.ac.at>
2023-03-07 23:14:14 +0100

    format code

--- exercise01/task1/myqueue.h
+++ exercise01/task1/myqueue.h
@@ -4,21 +4,14 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+static void myqueue_init(myqueue* q) {}
 
-static void myqueue_init(myqueue* q) {
-}
+static bool myqueue_is_empty(myqueue* q) {}
 
-static bool myqueue_is_empty(myqueue* q) {
-}
+static void myqueue_enqueue(myqueue* q, int value) {}
 
-static void myqueue_enqueue(myqueue* q, int value) {
-}
+static int myqueue_dequeue(myqueue* q) {}
 
-static int myqueue_dequeue(myqueue* q) {
-}
-
-static void myqueue_destroy(myqueue* q) {
-}
-
+static void myqueue_destroy(myqueue* q) {}
 
 #endif
--- exercise01/task1/myqueue_test.c
+++ exercise01/task1/myqueue_test.c
@@ -1,38 +1,37 @@
+#include "myqueue.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "myqueue.h"
 
 int main(int argc, char* argv[]) {
 
-    myqueue queue;
-    myqueue_init(&queue);
+	myqueue queue;
+	myqueue_init(&queue);
 
-    assert(myqueue_is_empty(&queue));
+	assert(myqueue_is_empty(&queue));
 
-    myqueue_enqueue(&queue, 1);
+	myqueue_enqueue(&queue, 1);
 
-    assert(!myqueue_is_empty(&queue));
+	assert(!myqueue_is_empty(&queue));
 
-    assert(1 == myqueue_dequeue(&queue));
+	assert(1 == myqueue_dequeue(&queue));
 
-    assert(myqueue_is_empty(&queue));
+	assert(myqueue_is_empty(&queue));
 
-    myqueue_enqueue(&queue, 1);
-    myqueue_enqueue(&queue, 2);
-    myqueue_enqueue(&queue, 3);
-    myqueue_enqueue(&queue, 4);
+	myqueue_enqueue(&queue, 1);
+	myqueue_enqueue(&queue, 2);
+	myqueue_enqueue(&queue, 3);
+	myqueue_enqueue(&queue, 4);
 
-    assert(1 == myqueue_dequeue(&queue));
-    assert(2 == myqueue_dequeue(&queue));
-    assert(3 == myqueue_dequeue(&queue));
+	assert(1 == myqueue_dequeue(&queue));
+	assert(2 == myqueue_dequeue(&queue));
+	assert(3 == myqueue_dequeue(&queue));
 
-    assert(!myqueue_is_empty(&queue));
+	assert(!myqueue_is_empty(&queue));
 
-    myqueue_destroy(&queue);
+	myqueue_destroy(&queue);
 
-    puts("All good!");
+	puts("All good!");
 
 	return EXIT_SUCCESS;
 }
-
